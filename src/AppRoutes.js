83b16be828965e4997347876efb8fd54d5b00a53
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2022-11-28 13:12:38 +0000

    deploy

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -3,14 +3,17 @@ import { Home } from "./Pages/Home";
 import { Career } from "./Pages/Career";
 import { ContactMe } from "./Pages/ContactMe";
 import { Portfolio } from "./Pages/Portfolio";
+import { BrowserRouter } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Portfolio" element={<Portfolio />} />
-      <Route path="/Career" element={<Career />} />
-      <Route path="/ContactMe" element={<ContactMe />} />
-    </Routes>
+    <BrowserRouter basename="/Portfolio">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        <Route path="/Career" element={<Career />} />
+        <Route path="/ContactMe" element={<ContactMe />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
