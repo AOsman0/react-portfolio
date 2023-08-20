f8d887b8c0632624912f06bb684e7499c7c95df4
AOsman0 <99136448+AOsman0@users.noreply.github.com>
2023-08-20 19:23:06 +0100

    hash-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./AppRoutes";
+import { HashRouter } from "react-router-dom";
 import { NavBar } from "./Components/NavBar";
 import { Footer } from "./Components/Footer";
 
 export const App = () => {
   return (
     <div className="bg-gradient-to-r from-gray-500">
-      <BrowserRouter>
+      <HashRouter>
         <NavBar />
         <AppRoutes />
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
     </div>
   );
